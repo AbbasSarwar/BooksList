f599656d744da3643d1f4b2f9a5f706ead6b96dc
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-03-21 15:18:52 +0300

    js classes

--- scripts.js
+++ scripts.js
@@ -4,37 +4,48 @@ const authorName = document.querySelector('#author');
 const btn = document.querySelector('form');
 
 const bookList = JSON.parse(localStorage.getItem('book-list')) || [];
-function addBook() {
-  const title = BookName.value;
-  const author = authorName.value;
-  const books = { title, author };
-  bookList.push(books);
-  localStorage.setItem('book-list', JSON.stringify(bookList));
-}
-btn.addEventListener('submit', addBook);
 
-// display book
-function displayBlock() {
-  container.innerHTML = '';
-  bookList.forEach((book, index) => {
-    container.innerHTML += `<ul class="Books">
-    <li class="inps">"${book.title}" by ${book.author}</li>
-    <li class="btnclass"><button class = "remove" onlick= "remove()" data-index="${index}">Remove</button></li>
-    </ul>`;
-  });
+class Book{
+  constructor() {
+    this.books = []
+  }
+
+  addBook() {
+    const title = BookName.value;
+    const author = authorName.value;
+    const books = { title, author };
+    bookList.push(books);
+    localStorage.setItem('book-list', JSON.stringify(bookList));
+  }
+
+  displayBlock() {
+    container.innerHTML = '';
+    bookList.forEach((book, index) => {
+      container.innerHTML += `
+      <ul class="Books">
+        <li class="inps">"${book.title}" by ${book.author}</li>
+        <li class="btnclass">
+          <button class = "remove" onlick= "remove()" data-index="${index}">Remove</button>
+        </li>
+        </ul>
+        `;
+      });
+    }
+    
+  remove(index) {
+    bookList.splice(index, 1);
+    localStorage.setItem('book-list', JSON.stringify(bookList));
+    book.displayBlock();
+  }
 }
 
-// remove function
-function remove(index) {
-  bookList.splice(index, 1);
-  localStorage.setItem('book-list', JSON.stringify(bookList));
-  displayBlock();
-}
-displayBlock();
+const book = new Book;
+btn.addEventListener('submit', book.addBook);
+book.displayBlock();
 
 container.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
     const index = e.target.getAttribute('data-index');
-    remove(index);
+    book.remove(index);
   }
 });
