b4b1fac21583464c342908ff98d47724b430482f
Abbas Sarwar <abbassarwar100@gmail.com>
2023-03-27 09:36:40 -0700

    Arrow functions implemented

--- modules/add-book.js
+++ modules/add-book.js
@@ -2,7 +2,7 @@ const BookName = document.querySelector('#book');
 const authorName = document.querySelector('#author');
 
 const bookList = JSON.parse(localStorage.getItem('book-list')) || [];
-export default function addBook() {
+export const addBook = ()=> {
   const title = BookName.value;
   const author = authorName.value;
   const books = { title, author };
--- modules/display-books.js
+++ modules/display-books.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 const container = document.querySelector('.collection');
 const bookList = JSON.parse(localStorage.getItem('book-list')) || [];
-export function displayBlock() {
+export const displayBlock = () => {
   container.innerHTML = '';
   bookList.forEach((book, index) => {
     container.innerHTML += `<ul class="Books">
